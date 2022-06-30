34dc17295aef5e6e6fc9fdeea69eca9aba827e1a
Annemarie <a.lucernoni0@gmail.com>
2022-06-30 15:56:14 -0700

    commit tree 3c8b768c52b8720d112d7e03d169cb2b9e2bebe9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    # validate :clickbait
+
+    # def clickbait
+    #     title_strings = ["Won't Believe", "Secret", "Top [number]" "Guess]
+    #     if 
+    #         title.include?[title_strings] = false
+    #             errors.add(:title, "must be more clickable!")
+    #     end
+        
+    # end
+
 end
